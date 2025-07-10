65da7ec9f8ccb0193d4e567d35799125bd5f1754
Borja Delgado Alcolea <121514643+borjadelgadodev@users.noreply.github.com>
2025-07-10 19:23:37 +0200

    feat: Add navigation between Main and Chat fragments with XML configuration

--- app/src/main/java/com/borjadelgadodev/chat/ui/chat/ChatFragment.kt
+++ app/src/main/java/com/borjadelgadodev/chat/ui/chat/ChatFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.borjadelgadodev.chat.R
 import com.borjadelgadodev.chat.databinding.FragmentChatBinding
 
 class ChatFragment : Fragment() {
@@ -14,8 +16,15 @@ class ChatFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentChatBinding.inflate(inflater, container, false)
+        binding.imageViewBack.setOnClickListener {
+            navigateToMain()
+        }
         return binding.root
+    }
+
+    private fun navigateToMain() {
+        findNavController().popBackStack()
     }
 }
--- app/src/main/java/com/borjadelgadodev/chat/ui/main/MainFragment.kt
+++ app/src/main/java/com/borjadelgadodev/chat/ui/main/MainFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.borjadelgadodev.chat.databinding.FragmentMainBinding
+import com.borjadelgadodev.chat.R
 
 class MainFragment : Fragment() {
 
@@ -16,6 +18,15 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentMainBinding.inflate(inflater, container, false)
+        binding.buttonStartChat.setOnClickListener {
+            if (binding.inputTextUserName.text.isNullOrEmpty().not()) {
+                navigateToChat()
+            }
+        }
         return binding.root
+    }
+
+    private fun navigateToChat() {
+        findNavController().navigate(R.id.action_fragment_main_to_fragment_chat)
     }
 }
